bf8c40b3d5aad09bcebe563fa75c25f94c11257c
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-02 20:18:47 +0200

    Function that convert lowercase characters into uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str: str) -> None:
     for ch in str:
-        print(chr(ord(ch) - 32) if 'a' <= ch <= 'z' else ch, end="")
+        print(
+            "{}".format(chr(ord(ch) - 32) if 'a' <= ch <= 'z' else ch), end=""
+        )
     print()
