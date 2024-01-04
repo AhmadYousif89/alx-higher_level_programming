0dc5bc246b532d6a9f8e164be3b9d7be38e7fb83
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-04 17:05:56 +0200

    Print list of arguments with a corresponding enumeration

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+import sys
+
+
+def main(argv: list[str]) -> None:
+    args_len: int = len(argv)
+    if args_len == 0:
+        print("0 arguments.")
+    if args_len > 0:
+        print("{0} argument{1}:".format(args_len, "s" if args_len > 2 else ""))
+        for i, arg in enumerate(argv, 1):
+            print("{0} : {1}".format(i, arg))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
