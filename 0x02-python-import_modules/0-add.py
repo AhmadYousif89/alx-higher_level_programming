e1a6dae569f42c28ff5c9ad62f70ba6500982115
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-04 14:21:04 +0200

    Import the function "add" and print the result of 1 + 2 = 3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,12 @@
-#!/usr/bin/python3
-import add_0
+#!/usr/bin/python3 -B
+from add_0 import add
 
-a = 1
-b = 2
-print("{0} + {1} = {2}".format(a, b, add_0.add(a, b)))
+
+def main() -> None:
+    a = 1
+    b = 2
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
+
+
+if __name__ == "__main__":
+    main()
