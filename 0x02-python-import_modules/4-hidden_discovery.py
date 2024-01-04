3fa043af35ff4ab3b816b6c7f7d9a323d2b7090a
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-04 20:47:50 +0200

    Print all the statments defined by the compiled module "hidden_4.pyc"

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+import hidden_4
+
+
+def main() -> None:
+    statments: list[str] = dir(hidden_4)
+
+    for statment in statments:
+        if statment[:2] == ("__"):
+            continue
+        print(statment)
+
+
+if __name__ == "__main__":
+    main()
