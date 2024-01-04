74cf872877f91312b5bbbdab95e4d3eb2d9fccff
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-04 21:37:03 +0200

    Replicate bytecode for "magic_calculation_102"

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+from magic_calculation_102 import add, sub
+
+
+def magic_calculation(a: int, b: int) -> int:
+    if a < b:
+        c: int = add(a, b)
+        for i in range(4, 7):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
