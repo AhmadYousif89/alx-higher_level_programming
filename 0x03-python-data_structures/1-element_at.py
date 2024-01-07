747c0a481988709e01246142823303c9f88a77e3
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-07 13:11:32 +0200

    Function that retrieves an element from a list like in C

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+from typing import Union
+
+
+def element_at(my_list: list[int], idx: int) -> Union[int, None]:
+    count: int = len(my_list)
+    if idx < 0 or idx >= count:
+        return None
+    for i in range(count):
+        if i == idx:
+            return my_list[i]
