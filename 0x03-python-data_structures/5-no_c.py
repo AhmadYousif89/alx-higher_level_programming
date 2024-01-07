4fad7044683041ac44bd4844c986c46b195dd899
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-07 13:39:01 +0200

    Function that removes all 'c's and 'C's from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for i in my_string:
-        if i is not 'c' and i is not 'C':
-            new_string += i
+    new_string = my_string.translate({ord(ch): None for ch in 'cC'})
     return new_string
