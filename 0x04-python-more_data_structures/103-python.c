9adeb869e61c74d7f1d0a82366cf7dff2b39f7d8
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-10 18:16:27 +0200

    Print some basic info about Python lists and Python bytes objects

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,5 +1,4 @@
 #include <Python.h>
-#include <stdio.h>
 
 /**
  * print_python_bytes - Print some basic info about Python bytes objects
@@ -7,24 +6,27 @@
  */
 void print_python_bytes(PyObject *p)
 {
-	printf("[.] bytes object info\n");
+	Py_ssize_t i, size, limit;
+	char *str;
 
+	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 
-	Py_ssize_t size = PyBytes_Size(p);
-	char *str = PyBytes_AsString(p);
+	str = ((PyBytesObject *)p)->ob_sval;
+	size = ((PyVarObject *)(p))->ob_size;
 
 	printf("  size: %ld\n", size);
 	printf("  trying string: %s\n", str);
 
-	printf("  first 10 bytes: ");
-	for (Py_ssize_t i = 0; i < size && i < 10; ++i)
-		printf("%02x ", (unsigned char)str[i]);
+	limit = size > 10 ? 10 : size + 1;
 
+	printf("  first %ld bytes: ", limit);
+	for (i = 0; i < size && i < limit; ++i)
+		printf("%02x ", (unsigned char)str[i]);
 	printf("\n");
 }
 
@@ -34,38 +36,21 @@ void print_python_bytes(PyObject *p)
  */
 void print_python_list(PyObject *p)
 {
+	Py_ssize_t i, size, allocated;
+
+	size = ((PyVarObject *)p)->ob_size;
+	allocated = ((PyListObject *)p)->allocated;
+
 	printf("[*] Python list info\n");
-
-	if (!PyList_Check(p))
-	{
-		printf("[!] Invalid List Object\n");
-		return;
-	}
-
-	Py_ssize_t size = PyList_Size(p);
-	Py_ssize_t allocated = ((PyListObject *)p)->allocated;
-
 	printf("[*] Size of the Python List = %ld\n", size);
 	printf("[*] Allocated = %ld\n", allocated);
 
-	for (Py_ssize_t i = 0; i < size; ++i)
+	for (i = 0; i < size; ++i)
 	{
-		printf("Element %ld: ", i);
-		PyObject *elem = PyList_GetItem(p, i);
+		PyObject *item = ((PyListObject *)p)->ob_item[i];
 
-		if (PyBytes_Check(elem))
-			print_python_bytes(elem);
-		else if (PyLong_Check(elem))
-			printf("int\n");
-		else if (PyFloat_Check(elem))
-			printf("float\n");
-		else if (PyTuple_Check(elem))
-			printf("tuple\n");
-		else if (PyList_Check(elem))
-			print_python_list(elem);
-		else if (PyUnicode_Check(elem))
-			printf("str\n");
-		else
-			printf("[!] Unknown type\n");
+		printf("Element %ld: %s\n", i, ((item)->ob_type)->tp_name);
+		if (PyBytes_Check(item))
+			print_python_bytes(item);
 	}
 }
