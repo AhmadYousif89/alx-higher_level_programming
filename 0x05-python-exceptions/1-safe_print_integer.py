a51c0ac6aea63c497b10fc86a84ead5987bb28a7
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-22 14:39:19 +0200

    Function that prints an integer and return true or false.

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def safe_print_integer(value):
+    is_int = isinstance(value, int)
+    try:
+        if is_int:
+            print("{0}".format(value))
+            return True
+    except:
+        return False
