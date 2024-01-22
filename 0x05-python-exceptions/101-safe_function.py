6925e32dd76390b6f5e3754032e6386a4c75539e
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-22 16:10:14 +0200

    Function that executes a function safely and write exception messages to stderr.

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+from sys import stderr
+
+
+def safe_function(fct, *args):
+    result = None
+
+    try:
+        result = fct(args[0], args[1])
+    except Exception as e:
+        print(f"Exception: {e}", file=stderr)
+
+    return result
