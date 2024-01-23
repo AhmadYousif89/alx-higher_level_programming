a5c6a7c40492604ae0850e8b5cbd1971efe969c6
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-23 17:13:54 +0200

    add the area method on the Square class

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Class that defines an empty Square"""
+
+
+class Square:
+    """Create a Square"""
+
+    __size = 0
+    """Initialize the Square class"""
+    """Args: (self, size=0) size type int"""
+
+    def __init__(self, size=0) -> None:
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size: int = size
+
+    def area(self) -> int:
+        """Calculate the area of the Square"""
+        return self.__size * self.__size
