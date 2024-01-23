34b7a6a56422362ac88a9ff34f580b9358b9fe75
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-23 18:52:06 +0200

    Define the get and set methods for the size attribute of the Square class

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""Class that defines an empty Square"""
+
+
+class Square:
+    """Create a Square"""
+
+    def __init__(self, size=0):
+        """Initialize the Square class"""
+        """Args: (self, size=0) size type int"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Get/Set the size of the Square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value=0):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self) -> int:
+        """Calculate the area of the Square"""
+        return self.__size * self.__size
