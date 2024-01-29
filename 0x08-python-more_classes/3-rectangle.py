342d71d3e1604d91b104d3777a1fd034cd7cb51a
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-29 16:39:58 +0200

    fix: correctly assigning the width and height values to their setter methods

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
         - width (optional): type int defaults to 0
         - height (optional): type int defaults to 0
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     def __str__(self):
         """
