845cd5880c09c6cc9e4afccfbed21d53741829ca
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-29 13:38:19 +0200

    upload: 3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Defines a say_my_name function"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Generate a string introducing a person with their first and last names.
+    Args:
+    - first_name (str): The first name of the person.
+    - last_name (str, optional): The last name of the person. Defaults to an empty string.
+    Returns:
+    - str: A string in the format "My name is <first_name> <last_name>".
+    Raises:
+    - TypeError: If either first_name or last_name is not a string.
+    """
+    if not type(first_name) == str:
+        raise TypeError("first_name must be a string")
+    if not type(last_name) == str:
+        raise TypeError("last_name must be a string")
+
+    intro = "My name is"
+    if first_name:
+        intro += ' ' + first_name.strip()
+    if last_name:
+        intro += ' ' + last_name.strip()
+    print(intro)
+
+
+# say_my_name("John", "Smith")
+# say_my_name("Walter", "White")
+# say_my_name("Bob")
+# try:
+#     say_my_name(12, "White")
+# except Exception as e:
+#     print(e)
