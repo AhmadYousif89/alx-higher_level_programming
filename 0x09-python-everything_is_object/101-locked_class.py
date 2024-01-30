6061e29049652a49a7971a0b371e46733ef8888e
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-30 18:21:11 +0200

    refactor: using the __slots__ variable

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,9 +7,4 @@ class LockedClass:
     This class creates new instances only for attributes called "first_name".
     """
 
-    def __setattr__(self, name, value):
-        if name != 'first_name':
-            raise AttributeError(
-                f"'LockedClass' object has no attribute '{name}'"
-            )
-        super().__setattr__(name, value)
+    __slots__ = ["first_name"]
