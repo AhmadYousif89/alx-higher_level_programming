25bcd920b146b2964600c92f0e9aec3f2950c1ce
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-01 20:05:25 +0200

    code refactor: using isinstance instead of type

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,9 +11,9 @@ def add_integer(a, b=98):
     Raises:
     - TypeError: If a or b is a non-integer and non-float.
     """
-    if not type(a) == int and not type(a) == float:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not type(b) == int and not type(b) == float:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
