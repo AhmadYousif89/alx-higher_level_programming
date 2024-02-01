d00ef9271cf5bb81479fd5aa136d0cb035e4b2fb
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-01 20:06:52 +0200

    code refactor: using isinstance instead of type and modified the "intro" variable

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -7,22 +7,19 @@ def say_my_name(first_name, last_name=""):
     Generate a string introducing a person with their first and last names.
     Args:
     - first_name (str): The first name of the person.
-    - last_name (str, optional): The last name of the person. Defaults to an empty string.
+    - last_name (str, optional): The last name of the person. Defaults to "".
     Returns:
     - str: A string in the format "My name is <first_name> <last_name>".
     Raises:
     - TypeError: If either first_name or last_name is not a string.
     """
-    if not type(first_name) == str:
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if not type(last_name) == str:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    intro = "My name is"
-    if first_name:
-        intro += ' ' + first_name.strip()
-    if last_name:
-        intro += ' ' + last_name.strip()
+    intro = f"My name is {first_name.strip()} {last_name.strip()}"
+
     print(intro)
 
 
