523b0e7104355b07baad5254a5626d482ebcf71c
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-01 22:11:24 +0200

    upload: 3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Add new element to the hash table.
+ * @ht: pointer to the hash table struct
+ * @key: the key
+ * @value: the value
+ * Return: 1 on success 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value) {}
