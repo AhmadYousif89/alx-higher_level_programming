57d5c3584ee605d13c908e6a6438d84db8963c03
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-01 22:10:32 +0200

    upload: 0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,8 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Create a Hash table
+ * @size: the size of the hash table's array
+ * Return: pointer to the newly created hash table or NULL otherwise.
+ */
+hash_table_t *hash_table_create(unsigned long int size) {}
