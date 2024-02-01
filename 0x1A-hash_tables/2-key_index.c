6bbf309316d7d86c691cd25a8e95ab8248739671
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-01 22:11:09 +0200

    upload: 2-key_index.c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,9 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Get the index of a key in the hash table.
+ * @key: the key used in the searching process
+ * @size: the size of the hash table's array
+ * Return: the index at which the key/value pair is stored
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size) {}
