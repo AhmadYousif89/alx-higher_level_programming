84ba1350adf819204aebd7c263408905b1c39a03
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-01 22:11:39 +0200

    upload: 4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieve a value associated with a key.
+ * @ht: pointer to the hash table struct
+ * @key: the key
+ * Return: the value associated with the element,
+ * 			or NULL if key couldn’t be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key) {}
