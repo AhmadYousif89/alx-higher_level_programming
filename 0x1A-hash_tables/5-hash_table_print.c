cd22e3b722aef6ec6bf7553ce8ab543ecb3f6fc0
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-02 16:14:58 +0200

    Delete wrong C folder

--- 0x1A-hash_tables/5-hash_table_print.c
+++
@@ -1,7 +0,0 @@
-#include "hash_tables.h"
-
-/**
- * hash_table_print - Print the hash table.
- * @ht: pointer to the hash table struct
- */
-void hash_table_print(const hash_table_t *ht) {}
--- 0x1A-hash_tables/6-hash_table_delete.c
+++
@@ -1,7 +0,0 @@
-#include "hash_tables.h"
-
-/**
- * hash_table_delete - Delete a hash table
- * @ht: pointer to the hash table struct
- */
-void hash_table_delete(hash_table_t *ht) {}
