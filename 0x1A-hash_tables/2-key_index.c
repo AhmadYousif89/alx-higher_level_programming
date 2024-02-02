890d920f3f0613c2d3ae05ba1dc16a37a96ef4fb
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-02 16:08:23 +0200

    Get the index of a key in the hash table.

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -6,4 +6,13 @@
  * @size: the size of the hash table's array
  * Return: the index at which the key/value pair is stored
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size) {}
+__uint64_t key_index(const unsigned char *key, __uint64_t size)
+{
+	__uint64_t index;
+
+	if (key == NULL || size == 0)
+		return (0);
+	index = hash_djb2(key) % size;
+
+	return index;
+}
