bf321e459199af9906142b088a648053e995d2de
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-05 23:57:14 +0200

    fix: the check for hasattr method

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -12,7 +12,7 @@ def add_attribute(obj, attr_name, attr_value):
     Raises:
     - TypeError: If the object cannot have a new attribute.
     """
-    if not hasattr(obj, "__dict__") and not hasattr(obj, "__slots__"):
+    if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
 
     setattr(obj, attr_name, attr_value)
