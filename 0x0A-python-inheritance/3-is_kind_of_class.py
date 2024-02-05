510a9c8b8873456c3c912f89f7e46c18a6775daf
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-05 23:18:06 +0200

    upload : 3-is_kind_of_class.py Checks if (obj) is instance of (a_class).

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines the is_kind_of_class function."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks if (obj) is instance of (a_class)"""
+    return isinstance(obj, a_class)
