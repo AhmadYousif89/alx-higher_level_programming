3318e0f6d8fe0f2afab52a3d13aa08b2917e4f43
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-05 20:55:30 +0200

    upload : 6-base_geometry.py Add method area.

---
+++ 0x0A-python_inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a BaseGeometry class."""
+
+
+class BaseGeometry:
+    """Creates the BaseGeometry class."""
+
+    def area(self):
+        """Calculates the area of the geometry shape."""
+        raise Exception("area() is not implemented")
