f8354d1a4742aef0bf3e477a924567e73e9593fd
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-07 17:00:35 +0200

    log stats.

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,56 +1,48 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics."""
+"""Define the Log parser functions."""
 import sys
-from collections import defaultdict
 
 
-def main():
+counter = 0
+total_size = 0
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
+}
+
+
+def log_stats():
     """
-    Reads input lines from sys.stdin, processes each line, and prints
-    statistics every 10 lines or after a keyboard interruption.
-
-
-    Metrics computed:
-    - Total file size
-    - Number of lines by status code (200, 301, 400, 401, 403, 404, 405, 500)
-    """
-    counter = 0
-    total_size = 0
-    status_codes = defaultdict(int)
-
-    try:
-        for line in sys.stdin:
-            line_segments = line.split()
-            total_size += int(line_segments[-1])
-            code = line_segments[-2]
-            status_codes[code] += 1
-            counter += 1
-            if counter % 10 == 0:
-                print_statistics(total_size, status_codes)
-        print_statistics(total_size, status_codes)
-    except EOFError:
-        print_statistics(total_size, status_codes)
-
-
-def print_statistics(size, status_codes):
-    """
-    Prints statistics with total file size and status code counts.
-
-    Parameters:
-    - size (int): Total file size.
-    - status_codes (dict): A Dict containing counts for different status codes.
+    Prints statistics based on the provided file size and status code dict.
 
     Prints:
     - File size: Total file size.
     - Number of lines for each status code.
     - Only prints statistics for status codes that have appeared in the input.
     """
-    print(f"File Size: {size}")
-    for key in sorted(status_codes.keys()):
-        count = status_codes[key]
-        if count > 0:
-            print(f"{key}: {count}")
+    print(f"File Size: {total_size}")
+    for key, value in sorted(status_codes.items()):
+        if value > 0:
+            print(f"{key}: {value}")
 
 
-if __name__ == "__main__":
-    main()
+try:
+    for line in sys.stdin:
+        line_segments = line.split()
+        if len(line_segments) >= 2:
+            total_size += int(line_segments[-1])
+            code = line_segments[-2]
+            if code in status_codes:
+                status_codes[code] += 1
+        counter += 1
+        if counter % 10 == 0:
+            log_stats()
+    log_stats()
+except KeyboardInterrupt:
+    log_stats()
