5a7cf8b18da34b2aeef4bf5da0a22c9526ed1d88
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-07 17:13:27 +0200

    log_stats function.

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,20 +1,5 @@
 #!/usr/bin/python3
 """Define the Log parser functions."""
-import sys
-
-
-counter = 0
-total_size = 0
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0,
-}
 
 
 def log_stats():
@@ -24,20 +9,37 @@ def log_stats():
     print("File Size: {}".format(total_size))
     for key, value in sorted(status_codes.items()):
         if value > 0:
-            print("{:s}: {:d}".format(key, value))
+            print("{}: {}".format(key, value))
 
 
-try:
-    for line in sys.stdin:
-        line_segments = line.split()
-        if len(line_segments) > 1:
-            total_size += int(line_segments[-1])
-            code = line_segments[-2]
-            if code in status_codes:
-                status_codes[code] += 1
-        counter += 1
-        if counter % 10 == 0:
-            log_stats()
-    log_stats()
-except KeyboardInterrupt:
-    log_stats()
+if __name__ == "__main__":
+    import sys
+
+    counter = 0
+    total_size = 0
+    status_codes = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
+    }
+
+    try:
+        for line in sys.stdin:
+            line_segments = line.split()
+            if len(line_segments) > 1:
+                total_size += int(line_segments[-1])
+                code = line_segments[-2]
+                if code in status_codes:
+                    status_codes[code] += 1
+            counter += 1
+            if counter % 10 == 0:
+                log_stats()
+        log_stats()
+    except KeyboardInterrupt:
+        log_stats()
+        raise
