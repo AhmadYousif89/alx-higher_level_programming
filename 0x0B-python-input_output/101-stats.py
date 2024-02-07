a8702a302147ede04795f0d77f718e02a3ae79c4
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-07 17:38:41 +0200

    Log parser function

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,19 +6,27 @@ def log_stats():
     """
     Prints statistics based on the provided file size and status code dict.
     """
-    print("File size: {}".format(size))
+    print(f"File size: {size}")
     for key, value in sorted(status_codes.items()):
         if value > 0:
-            print("{}: {}".format(key, value))
+            print(f"{key}: {value}")
 
 
 if __name__ == "__main__":
     import sys
-    from collections import defaultdict
 
     size = 0
     count = 0
-    status_codes = defaultdict(int)
+    status_codes = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
+    }
 
     try:
         for line in sys.stdin:
@@ -26,7 +34,9 @@ if __name__ == "__main__":
             if len(line_segments) > 1:
                 size += int(line_segments[-1])
                 code = line_segments[-2]
-                status_codes[code] += 1
+                if code in status_codes:
+                    status_codes[code] += 1
+
             count += 1
             if count % 10 == 0:
                 log_stats()
