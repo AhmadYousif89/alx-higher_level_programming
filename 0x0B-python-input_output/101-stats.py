8b2dc6ed9bdefc04db20b80488781f60275af4de
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-07 16:20:52 +0200

    code refactor: start with empty status_code dict

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -15,13 +15,16 @@ def main():
     """
     counter = 0
     total_size = 0
-    status_codes = defaultdict(int)
+    status_codes = {}
     try:
         for line in sys.stdin:
             line_segments = line.split()
             total_size += int(line_segments[-1])
             code = line_segments[-2]
-            status_codes[code] += 1
+            if code in status_codes:
+                status_codes[code] += 1
+            else:
+                status_codes[code] = 0
             counter += 1
             if counter % 10 == 0:
                 print_statistics(total_size, status_codes)
