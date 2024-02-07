a5fc361d385d265653063a44d3cce2d3dd17bf7d
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-07 17:32:56 +0200

    test 4

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -7,8 +7,9 @@ def log_stats():
     Prints statistics based on the provided file size and status code dict.
     """
     print("File size: {}".format(size))
-    for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key]))
+    for key, value in sorted(status_codes.items()):
+        if value > 0:
+            print("{}: {}".format(key, value))
 
 
 if __name__ == "__main__":
@@ -16,8 +17,17 @@ if __name__ == "__main__":
 
     size = 0
     count = 0
-    status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    status_codes = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
+    }
+    # valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 
     try:
         for line in sys.stdin:
@@ -25,11 +35,11 @@ if __name__ == "__main__":
             if len(line_segments) > 1:
                 size += int(line_segments[-1])
                 code = line_segments[-2]
-                if code in valid_codes:
-                    if status_codes.get(code, -1) == -1:
-                        status_codes[code] = 1
-                    else:
-                        status_codes[code] += 1
+                if code in status_codes:
+                    # if status_codes.get(code, -1) == -1:
+                    #     status_codes[code] = 1
+                    # else:
+                    status_codes[code] += 1
             count += 1
             if count % 10 == 0:
                 log_stats()
