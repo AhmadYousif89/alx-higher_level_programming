9a04c3564e5f8b536f86436d098c3416acb5518e
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-04-09 21:48:35 +0200

    upload: 102-concat.js

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const rf1 = process.argv[2];
+const rf2 = process.argv[3];
+const wf = process.argv[4];
+
+if (!rf1 || !rf2 || !wf) {
+  process.exit(1);
+}
+
+try {
+  const rf1Data = fs.readFileSync(rf1);
+  const file2Data = fs.readFileSync(rf2);
+  fs.writeFileSync(wf, `${rf1Data}\n${file2Data}`);
+} catch (err) {
+  console.error(err);
+}
