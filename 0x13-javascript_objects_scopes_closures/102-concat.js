de6d29da29fb7e0c9d9dcbe5c81695f4430ca0ca
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-04-09 22:02:58 +0200

    fix: remove try/catch

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,10 +6,11 @@ const rf1 = process.argv[2];
 const rf2 = process.argv[3];
 const wf = process.argv[4];
 
-try {
-  const rf1Data = fs.readFileSync(rf1);
-  const file2Data = fs.readFileSync(rf2);
-  fs.writeFileSync(wf, `${rf1Data}\n${file2Data}`);
-} catch (err) {
-  console.error(err);
+if (!rf1 || !rf2 || !wf) {
+  process.exit(1);
 }
+
+const rf1Data = fs.readFileSync(rf1);
+const file2Data = fs.readFileSync(rf2);
+fs.writeFileSync(wf, `${rf1Data}\n${file2Data}`);
+
