abdb0fa38a56d3f75e2f8f6961419ce05cf53033
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-04-13 00:47:06 +0200

    upload: 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Lists a specific state from the database hbtn_0e_0_usa"""
+
+
+def main():
+    HOST = "localhost"
+    PORT = 3306
+    USER = sys.argv[1]
+    PASSWD = sys.argv[2]
+    DB = sys.argv[3]
+    db = MySQLdb.connect(host=HOST, port=PORT, user=USER, passwd=PASSWD, db=DB)
+    cur = db.cursor()
+    sql = "SELECT * FROM states WHERE name LIKE 'N%'"
+    cur.execute(sql)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    main()
