ea4747aa73448fe8325caa3826bbf242805e875f
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-04-13 00:47:24 +0200

    upload: 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Lists a specific state from the database hbtn_0e_0_usa"""
+"""while handling SQL injections"""
+
+
+def main():
+    HOST = "localhost"
+    PORT = 3306
+    USER = sys.argv[1]
+    PASSWD = sys.argv[2]
+    DB = sys.argv[3]
+    db = MySQLdb.connect(host=HOST, port=PORT, user=USER, passwd=PASSWD, db=DB)
+    cur = db.cursor()
+    target = sys.argv[4]
+    sql = "SELECT * FROM states WHERE name = '{}'".format(target)
+    cur.execute(sql)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    main()
