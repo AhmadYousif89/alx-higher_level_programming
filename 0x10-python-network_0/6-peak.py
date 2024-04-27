9a86bc9b72bf8314a5f91816fb6544df98f41975
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-04-27 03:44:08 +0300

    find peak element

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,33 +1,51 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Get the peak element from an unsorted list"""
 
 
-def find_peak(A):
-    """find pick element"""
-    if A == []:
+def get_peak_recursivly(int_list, left, right):
+    """
+    Finds the peak element in the list recursively.
+
+    Args:
+        int_list: The list of integers to search.
+        peak: The index of the current element being considered as a peak.
+
+    Returns:
+        The peak element in the list, or None if the list is empty.
+    """
+    mid = (left + right) // 2
+    peak = int_list[mid]
+    prev_n = int_list[mid - 1]
+    next_n = int_list[mid + 1] if mid < len(int_list) - 1 else 0
+    # check if the middle element is greater than its neighbors
+    if (mid == 0 or prev_n <= peak) and (
+        mid == len(int_list) - 1 or next_n <= peak
+    ):
+        return peak
+
+    # If the left neighbor of `mid` is greater than the middle element,
+    # find the peak recursively in the left sublist
+    if mid - 1 >= 0 and prev_n > peak:
+        return get_peak_recursivly(int_list, left, mid - 1)
+    # If the right neighbor of `mid` is greater than the middle element,
+    # find the peak recursively in the right sublist
+    return get_peak_recursivly(int_list, mid + 1, right)
+
+
+def find_peak(list_of_integers):
+    """
+    Finds the peak element in the list.
+
+    Args:
+        list_of_integers: The list of integers to search.
+
+    Returns:
+        The peak element in the list, or None if the list is empty.
+    """
+    if list_of_integers is None or not list_of_integers:
         return None
 
-    def recursive(A, left=0, right=len(A) - 1):
-        """helper recursive function"""
-
-        mid = (left + right) // 2
-
-        # check if the middle element is greater than its neighbors
-        if (mid == 0 or A[mid - 1] <= A[mid]) and (
-            mid == len(A) - 1 or A[mid + 1] <= A[mid]
-        ):
-            return A[mid]
-
-        # If the left neighbor of `mid` is greater than the middle element,
-        # find the peak recursively in the left sublist
-        if mid - 1 >= 0 and A[mid - 1] > A[mid]:
-            return recursive(A, left, mid - 1)
-
-        # If the right neighbor of `mid` is greater than the middle element,
-        # find the peak recursively in the right sublist
-        return recursive(A, mid + 1, right)
-
-    return recursive(A)
+    return get_peak_recursivly(list_of_integers, 0, len(list_of_integers))
 
 
 if __name__ == "__main__":
