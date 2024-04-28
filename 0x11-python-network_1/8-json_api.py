c7293f79bf7152ed1ac2da1f12dedbbde17befb8
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-04-28 15:15:28 +0300

    upload: 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a query parameter.
+"""
+import sys
+import requests
+
+url = 'http://0.0.0.0:5000/search_user'
+query = sys.argv[1] if len(sys.argv) > 1 else ''
+if not query:
+    exit(1)
+
+try:
+    res = requests.post(url, data={"q": query})
+    data = res.json()
+    if res.ok and data:
+        print(f"[{data['id']}] {data['name']}")
+    else:
+        print("No result")
+except ValueError:
+    print("Not a valid JSON")
