e69aefb999c93e584e4cedd4dcea8aa0039209c0
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-04-28 15:14:56 +0300

    upload: 4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status"""
+
+import requests
+
+url = 'https://alx-intranet.hbtn.io/status'
+res = requests.get(url)
+print("Body response:")
+print("\t- type:", type(res.text))
+print("\t- content:", res.text)
