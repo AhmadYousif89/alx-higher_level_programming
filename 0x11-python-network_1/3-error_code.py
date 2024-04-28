6e53ff4e4aa1aa2738cc8698a8a5601d6b7f9729
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-04-28 15:14:49 +0300

    upload: 3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL, sends a request to the URL and displays the body
+of the response (decoded in utf-8).
+"""
+
+import sys
+from urllib import request
+from urllib.error import HTTPError, URLError
+
+url = sys.argv[1] if len(sys.argv) > 1 else ''
+if not url:
+    exit(1)
+
+try:
+    with request.urlopen(url) as response:
+        res = response.read().decode('utf-8')
+        print(res)
+except HTTPError as e:
+    print('Error code:', e.code)
