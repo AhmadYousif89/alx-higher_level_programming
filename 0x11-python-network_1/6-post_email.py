b639604c9ed19d8d40dd6aec2eb730601b27c803
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-04-28 15:15:12 +0300

    upload 6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL, sends a request to the URL and displays 
+the value of the variable X-Request-Id in the response header
+"""
+
+import sys
+import requests
+
+url = sys.argv[1] if len(sys.argv) > 1 else ''
+email = sys.argv[2] if len(sys.argv) > 2 else ''
+if not url or not email:
+    exit(1)
+
+data = {"email": email}
+res = requests.post(url, data)
+print(res.text)
