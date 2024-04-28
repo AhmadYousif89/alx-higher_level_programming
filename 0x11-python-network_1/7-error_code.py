73425a5fdd80da25c30634c7bc800f65cb68a5e3
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-04-28 15:15:19 +0300

    upload: 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL, sends a request to the URL and displays 
+the value of the variable X-Request-Id in the response header
+"""
+import sys
+import requests
+
+url = sys.argv[1] if len(sys.argv) > 1 else ''
+if not url:
+    exit(1)
+
+try:
+    res = requests.get(url)
+    if res.ok:
+        print(res.text)
+    else:
+        res.raise_for_status()
+except requests.exceptions.HTTPError as e:
+    print("Error code:", e.response.status_code)
