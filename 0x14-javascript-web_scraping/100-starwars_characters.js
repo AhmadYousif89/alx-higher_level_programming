e5d736edbe1546d241731edd45859bcf3df9ae51
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-05-21 18:14:37 +0300

    Add script to print Star Wars characters

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// script that prints all characters of a Star Wars movie
+
+const request = require('request');
+
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const characters = JSON.parse(body).characters;
+  characters.forEach(character_url =>
+    request(character_url, (err, res, body) =>
+      err ? console.log(err) : console.log(JSON.parse(body).name)
+    )
+  );
+});
