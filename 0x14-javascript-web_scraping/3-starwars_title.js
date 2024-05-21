f9442dd884495f6000193a8afd707a252e9f45ec
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-05-21 18:34:19 +0300

    Update console.log to display only the title of the film

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,5 +6,5 @@ const request = require('request');
 const episode = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${episode}`;
 request(url, (err, res, body) =>
-  err ? console.log(err) : console.log(JSON.parse(body))
+  err ? console.log(err) : console.log(JSON.parse(body).title)
 );
