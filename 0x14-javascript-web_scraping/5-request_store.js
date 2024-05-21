97392337284fdf9c1045742ca0957d095ccb5307
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-05-21 18:57:59 +0300

    Refactor request handling in 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,11 +7,4 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, (err, res, body) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-
-  fs.writeFile(filePath, body, 'utf8', () => {});
-});
+request(url).pipe(fs.createWriteStream(filePath));
