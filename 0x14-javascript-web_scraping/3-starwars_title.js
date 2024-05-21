071ab842ffb37c72c6517c1358a07092b6e2c2b1
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-05-21 18:31:47 +0300

    Update API URL in 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,7 @@
 const request = require('request');
 
 const episode = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${episode}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${episode}`;
 request(url, (err, res, body) =>
   err ? console.log(err) : console.log(JSON.parse(body))
 );
