a758c732b2b226205bd2269f70b2b030ddb7871d
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-05-21 18:14:28 +0300

    Add script to get webpage contents and store in a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// script that gets the contents of a webpage and stores it in a file.
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const file_path = process.argv[3];
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  fs.writeFile(file_path, body, { encoding: 'utf8', flag: 'w' }, err =>
+    err ? console.log(err) : console.log(`The file was saved!`)
+  );
+});
