dbd7bdc073140109c81874557daf4dc5e4b97f50
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-05-29 23:52:09 +0300

    Update API URL and add language code example

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,10 +1,13 @@
 // script that fetches and prints how to say “Hello” depending on the language
 
 const $ = window.$;
-const url = 'https://www.fourtonfish.com/hellosalut/hello/';
+const url = 'https://hellosalut.stefanbohacek.dev/';
 $(function () {
+  $('#btn_translate').after('<small id="lang-ex">Ex : ar, fr, en</small>');
+  $('#lang-ex').css({ display: 'block', color: '#555' });
   $('#btn_translate').click(() => {
+    $('#hello').empty();
     const lang = $('#language_code').val();
-    $.get(`${url}?lang=${lang}`, data => $('#hello').text(data.hello));
+    $.get(`${url}?lang=${lang}`, data => $('#hello').append(data.hello));
   });
 });
